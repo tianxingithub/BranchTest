7feff314dea7bb179ada33d2098bc8fc414ec7cd
HSHub <HanShanGithub@github.com>
2024-07-21 00:42:36 +0800

    修改Ovserver内容

--- src/HSHub/WsfCyberObserver.cpp
+++ src/HSHub/WsfCyberObserver.cpp
@@ -1,16 +1,7 @@
-// ****************************************************************************
+/ ****************************************************************************
 // CUI
 //
-// The Advanced Framework for Simulation, Integration, and Modeling (AFSIM)
-//
-// Copyright 2016 Infoscitex, a DCS Company. All rights reserved.
-//
-// The use, dissemination or disclosure of data in this file is subject to
-// limitation or restriction. See accompanying README and LICENSE for details.
-// ****************************************************************************
-
-#include "WsfCyberObserver.hpp"
-
+// The 
 #include "WsfCyberSimulationExtension.hpp"
 
 #define WSF_CYBER_OBSERVER_CALLBACK_DEFINE(EVENT)                                       \
--- src/HSHub/WsfCyberObserver.hpp
+++ src/HSHub/WsfCyberObserver.hpp
@@ -1,11 +1,6 @@
 // ****************************************************************************
 // CUI
-//
-// The Advanced Framework for Simulation, Integration, and Modeling (AFSIM)
-//
-// Copyright 2016 Infoscitex, a DCS Company. All rights reserved.
-//
-// The use, dissemination or disclosure of data in this file is subject to
+
 // limitation or restriction. See accompanying README and LICENSE for details.
 // ****************************************************************************
 
